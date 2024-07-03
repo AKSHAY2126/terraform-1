7ad419edc0c250d73e4f570d78e28d749449bbbf
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-07-03 20:52:03 +0530

    add storage account and network

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     resource_group_name  = "myrg-1"
     storage_account_name = "v50a7877jlqtgxdoytqcu7bh"
     container_name       = "rmtx1z0t5bmz51cg"
-    key                  = "prod.terraform.tfstate"
   }
 }
