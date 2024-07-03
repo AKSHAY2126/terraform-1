58b7f99b167ab8919bebfbfc895ff8c6b5f55bda
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-07-03 15:17:40 +0530

    storage account update

--- storage.tf
+++ storage.tf
@@ -2,16 +2,12 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "existing" {
-  name = "myrg-1"
-}
-
 module "storage_account" {
   source = "./module-1"
 
   storage_account_name   = var.storage_account_name
-  resource_group_name    = data.azurerm_resource_group.existing.name  # Reference the existing resource group
-  location               = data.azurerm_resource_group.existing.location  # Reference the existing resource group's location
+  resource_group_name    = azurerm_resource_group.myrg.name  # Reference the existing resource group
+  location               = azurerm_resource_group.myrg.location  # Reference the existing resource group's location
 }
 
 resource "random_id" "random_suffix" {
