7e503662ebdb3f34d28471f2eeffdc69f1f74fbe
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-07-03 15:31:22 +0530

    storage account update

--- module-1/storage.tf
+++ module-1/storage.tf
@@ -1,5 +1,10 @@
+resource "random_string" "suffix" {
+  length  = 6  # Adjust the length as needed
+  special = false
+}
+
 resource "azurerm_storage_account" "demo1" {
-  name                     = var.storage_account_name
+  name                     = "${var.storage_account_name}-${random_string.suffix.result}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
