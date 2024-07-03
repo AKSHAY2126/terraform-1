f112e92c3f6a4d2c0504aa7a46843ca5a574f176
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-07-03 15:14:12 +0530

    storage account update

--- storage.tf
+++ storage.tf
@@ -1,26 +1,17 @@
-
 provider "azurerm" {
   features {}
 }
 
-# Import existing resource group
-resource "azurerm_resource_group" "myrg" {
-  name     = "myrg-1"
-  location = "central India"
-
-  # This block is for importing an existing resource group
-  lifecycle {
-    ignore_changes = [tags]
-  }
+data "azurerm_resource_group" "existing" {
+  name = "myrg-1"
 }
 
-# Define other resources, such as storage account and storage container
 module "storage_account" {
   source = "./module-1"
 
   storage_account_name   = var.storage_account_name
-  resource_group_name    = azurerm_resource_group.myrg.name  # Reference the existing resource group
-  location               = azurerm_resource_group.myrg.location  # Reference the existing resource group's location
+  resource_group_name    = data.azurerm_resource_group.existing.name  # Reference the existing resource group
+  location               = data.azurerm_resource_group.existing.location  # Reference the existing resource group's location
 }
 
 resource "random_id" "random_suffix" {
